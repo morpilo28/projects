60da5d1297c1af15de4311c8996e4663d5fedf7f
mor <odkarit22@gmail.com>
2019-08-14 14:39:22 +0300

    project updated - 14.08.19 14:39

---
+++ project/temp-main.js
@@ -0,0 +1,199 @@
+/* 
+1. need to continue from p.6 - save to local storage.
+2. make everything jquery?
+3. needs to adjust for mobile
+*/
+"use strict";
+var interval;
+function main() {
+    homePage();
+    jQuery("#home").click(homePage);
+    jQuery("#liveReports").click(liveReportsPage);
+    jQuery("#about").click(aboutPage);
+}
+
+function homePage() { //creating the home page
+    jQuery('#currentPage').empty();
+    let currentPage = document.getElementById('currentPage');
+    let row = document.createElement('div');
+    row.id = 'row';
+    currentPage.appendChild(row);
+
+    jQuery('.background').css('height', '416%');
+    getAllCoins();
+}
+
+function liveReportsPage() { //creating the live reports page
+    jQuery('#currentPage').empty();
+}
+
+function aboutPage() { //creating the about page
+    jQuery('#currentPage').empty().append(`<div> <p> <span> <u> About Myself </u> </span> <br>
+    <img width = 152 height = 202 opacity=1 src = styles/images/mor.jpg> </img> <br> <br>
+    <span> My name is Mor. <br> I'm 28 years old.<br> I came to lear fullstack so i could fine a 
+    decent job but i don't know if it's going to happend. </span> <br> </p> <br>
+    <p> <span> <u> The Project </u> <br> this project is making me insane. </span> </p> </div>`);
+}
+
+function getAllCoins() {
+    jQuery.get("https://api.coingecko.com/api/v3/coins/list", function (result) {
+        let first100coins = getCoinCards(result);
+
+        for (let i = 0; i < 100; i++) {
+            let click = 0;
+            jQuery('#button' + i).click(function () {
+                //document.getElementById('button' + i).addEventListener('click', function () {
+                if (click == 0) {
+                    progressBar(i); //progress bar appears
+                    jQuery.get("https://api.coingecko.com/api/v3/coins/" + first100coins[i].id, function (coinInfo) {
+                        let info = document.getElementById('info' + i);
+
+                        //setTimeout(() => {
+                        progressBarFinished(i); //progress bar stops                            
+
+                        let img = document.createElement('img');
+                        img.src = coinInfo.image.small
+
+                        let value = document.createElement('div');
+                        let coinValue = `<b>USD:</b> ${coinInfo.market_data.total_volume.usd} &#36<br>
+                    <b>EUR:</b> ${coinInfo.market_data.total_volume.eur} \u20AC<br>
+                    <b>ILS:</b> ${coinInfo.market_data.total_volume.ils} &#8362`;
+                        value.innerHTML = coinValue;
+
+                        info.appendChild(img);
+                        info.appendChild(value);
+
+                        window.localStorage.setItem('img' + i, JSON.stringify(img.src));
+                        window.localStorage.setItem('coinValue' + i, JSON.stringify(coinValue));
+                        //}, 5000);
+                    });
+                    click++;
+                } else {
+                    jQuery('#info' + i).empty();
+                    click = 0;
+                }
+            });
+        }
+    });
+}
+
+function getCoinCards(result) {
+    let first100coins = [];
+    for (let i = 0; i < 100; i++) {
+        let cardGrid = document.createElement('div');
+        cardGrid.className = "col-md-3";
+        row.appendChild(cardGrid);
+       
+        let card = document.createElement('div');
+        card.className = 'card w-100';
+        cardGrid.appendChild(card);
+       
+        let cardBody = document.createElement('div');
+        cardBody.id = 'cardBody' + i;
+        cardBody.classList.add('card-body', 'border-primary', 'mb-3');
+        card.appendChild(cardBody);
+
+        //toggleSwitch(cardBody, i);
+
+        let cardHeader = document.createElement('h2');
+        cardHeader.className = 'card-title';
+        cardBody.appendChild(cardHeader);
+        cardHeader.innerText = result[i].symbol;
+        
+        let cardSubtitle = document.createElement('h7');
+        cardSubtitle.className = 'card-subtitle';
+        cardBody.appendChild(cardSubtitle);
+        cardSubtitle.innerHTML = result[i].name + '<br><br>';
+        
+        let infoButton = document.createElement('button');
+        infoButton.className = 'info-button';
+        infoButton.setAttribute('data-toggle', 'collapse');
+        infoButton.setAttribute('data-target', '#info' + i);
+        infoButton.id = 'button' + i;
+        cardBody.appendChild(infoButton);
+        infoButton.innerHTML = 'More Info';
+        
+        let info = document.createElement('div');
+        info.id = 'info' + i;
+        info.className = 'collapse';
+        cardBody.appendChild(info);
+       
+        first100coins.push(result[i]);
+    }
+    return first100coins;
+}
+
+function progressBar(i) {
+    let info = document.getElementById('info' + i);
+    let loading = document.createElement('div');
+    loading.id = 'loading';
+    loading.innerHTML = '<p>Loading...</p>'
+    info.appendChild(loading);
+
+    let a = document.createElement('div');
+    a.id = 'progressBarContainer';
+    a.classList.add('progress');
+    loading.appendChild(a);
+
+    let b = document.createElement('div');
+    b.id = 'progressBar' + i;
+    b.classList.add('progress-bar', 'progress-bar-striped', 'progress-bar-animated');
+    b.setAttribute('role', 'progressbar');
+    b.setAttribute('aria-valuenow', '0');
+    b.setAttribute('aria-valuemin', '0');
+    b.setAttribute('aria-valuemax', '100');
+    a.appendChild(b);
+
+    let width = 0;
+    document.getElementById('progressBar' + i).style.width = width + '%';
+    interval = setInterval(function () {
+        width += 10;
+        document.getElementById('progressBar' + i).style.width = width + '%';
+    }, 1000);
+}
+
+function progressBarFinished(i) {
+    let progressBar = document.getElementById('progressBar' + i);
+    progressBar.style.width = 100 + '%';
+    clearInterval(interval);
+}
+
+function toggleSwitch(cardBody, i) {
+    let buttonSwitch = document.createElement('div');
+    buttonSwitch.classList.add('toggle', 'btn', 'btn-dark'); // to switch to off - buttonSwitch.classList.add('toggle','btn','btn-light', 'off');
+    buttonSwitch.setAttribute('data-toggle', 'toggle');
+    buttonSwitch.setAttribute('role', 'button');
+    buttonSwitch.setAttribute('style', 'width:61.0833px');
+    buttonSwitch.setAttribute('style', 'height:38px');
+    cardBody.appendChild(buttonSwitch);
+    
+    let cardSwitch = document.createElement('input');
+    cardSwitch.id = 'cardSwitch' + i;
+    cardSwitch.setAttribute('type', 'checkbox');
+    //cardSwitch.setAttribute('checked', 'true');
+    cardSwitch.setAttribute('data-toggle', 'toggle');
+    cardSwitch.setAttribute('data-size', 'xs');
+    cardSwitch.setAttribute('data-onstyle', 'dark');
+    buttonSwitch.appendChild(cardSwitch);
+    
+    let toggle = document.createElement('div');
+    toggle.classList.add('toggle-group');
+    buttonSwitch.appendChild(toggle);
+    
+    let label1 = document.createElement('label');
+    label1.classList.add('btn', 'btn-dark', 'toggle-on');
+    label1.innerText = 'On';
+    toggle.appendChild(label1);
+    
+    let label2 = document.createElement('label');
+    label2.classList.add('btn', 'btn-light', 'toggle-off');
+    label2.innerText = 'Off';
+    toggle.appendChild(label2);
+    
+    let span = document.createElement('span');
+    span.classList.add('toggle-handle', 'btn', 'btn-light');
+    span.innerText = 'Off';
+    toggle.appendChild(span);
+}
+
+main();
