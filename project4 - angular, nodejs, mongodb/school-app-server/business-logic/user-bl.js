c0ecef0b613f55586283908c066bdc2be5327d59
mor <odkarit22@gmail.com>
2020-02-27 19:06:29 +0200

    TODO

--- project4 - angular, nodejs, mongodb/school-app-server/business-logic/user-bl.js
+++ project4 - angular, nodejs, mongodb/school-app-server/business-logic/user-bl.js
@@ -1,6 +1,10 @@
-//get('administrator').then(res => console.log(res)); // from bl
-//getOne('administrator', "5e57cf70b97e27183cd46a6c").then(res => console.log(res)); // from bl 
-//insert('administrator', { "name": "oz", "role": "sales" }).then(res => console.log(res)); // from bl 
-//update('administrator', { "_id": "5e57db8de8e843269831a5a6", "name": "solki", "role": "bitch" }).then(res => console.log(res)); // from bl 
-//deleteDocument('administrator', "5e57f0dcd50ad031e09207f4").then(res => console.log("id dDeleted: "+ res)); // from bl 
+//TODO: create functions and put each code line in his matching function:
+    //get('administrator').then(res => console.log(res)); // from bl
+    //getOne('administrator', "5e57cf70b97e27183cd46a6c").then(res => console.log(res)); // from bl 
+    //insert('administrator', { "name": "oz", "role": "sales" }).then(res => console.log(res)); // from bl 
+    //update('administrator', { "_id": "5e57db8de8e843269831a5a6", "name": "solki", "role": "bitch" }).then(res => console.log(res)); // from bl 
+    //deleteDocument('administrator', "5e57f0dcd50ad031e09207f4").then(res => console.log("id dDeleted: "+ res)); // from bl 
 
+"use strict";
+const dal = require('../dal');
+const collection = 'administrator';
