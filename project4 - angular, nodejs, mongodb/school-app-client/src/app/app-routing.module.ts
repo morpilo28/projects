7164f8a4f3a047c866d7557edf958dff5b7aa3d3
mor <odkarit22@gmail.com>
2020-02-28 01:29:49 +0200

    generated components: administration, login, school, user info

--- project4 - angular, nodejs, mongodb/school-app-client/src/app/app-routing.module.ts
+++ project4 - angular, nodejs, mongodb/school-app-client/src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { SchoolComponent } from './components/school/school.component';
+import { AdministrationComponent } from './components/administration/administration.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'school', component: SchoolComponent },
+  { path: 'administration', component: AdministrationComponent },
+  { path: '', redirectTo: '/login', pathMatch: 'full' }, //TODO: home component depends on if user is connected
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- project4 - angular, nodejs, mongodb/school-app-client/src/app/app.module.ts
+++ project4 - angular, nodejs, mongodb/school-app-client/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { SchoolComponent } from './components/school/school.component';
+import { UserInfoComponent } from './components/user-info/user-info.component';
+import { AdministrationComponent } from './components/administration/administration.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    SchoolComponent,
+    UserInfoComponent,
+    AdministrationComponent
   ],
   imports: [
     BrowserModule,
